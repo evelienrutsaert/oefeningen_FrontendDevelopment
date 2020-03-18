bcea46a1906c299db2cea857bf7dc43d799ca932
evelienrutsaert <evelien.rutsaert@arteveldehs.be>
2020-03-18 11:24:31 +0100

    wip

---
+++ bitterbal_evr/js/script.js
@@ -0,0 +1,19 @@
+// 1. plaats beide waardes: startwaarde en eindwaarde in een variabele (7 en 100)
+
+//2. Maak een loop die loopt van startwaarde 7, tot eindwaarde 100. 
+//M.A.W. tot zolang de startwaarde kleiner is dan de eindwaarde
+    //3. Ga binnen de loop na als de  startwaarde op dat moment deelbaar is door 3 EN als die deelbaar is door 5
+    //TIP: gerbuik restwaarde %
+
+
+
+// Fancy
+// 1. Vraag de startwaarde, eindwaarde, en twee getallen op via prompt
+// 2. Maak een loop van de startwaarde tot de eindwaarde
+        //Controleer indien de huidige startwaarde deelbaar is door getal 1 EN 2
+        // Schrijf bitterbal
+        //Controleer indien de huidige startwaarde deelbaar is door getal 1
+        //Schrijf bitter
+        //Controleer indien de huidige startwaarde deelbaar is door getal 2
+         //Schrijf bal
+         //Anders schrijf je het getal
